e0102adc056c13e2adcc6f82d8bec9452d28a83d
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-09-14 22:43:37 -0300

    email validate

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,6 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../helpers/validators.dart';
+
 class LoginScreen extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,63 +18,77 @@ class LoginScreen extends StatelessWidget {
       body: Center(
         child: Card(
           margin: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: ListView(
-            padding: const EdgeInsets.all(16.0),
-            shrinkWrap: true,
-            children: [
-              TextFormField(
-                autocorrect: false,
-                decoration: const InputDecoration(
-                  hintText: 'E-mail',
+          child: Form(
+            key: formKey,
+            child: ListView(
+              padding: const EdgeInsets.all(16.0),
+              shrinkWrap: true,
+              children: [
+                TextFormField(
+                  autocorrect: false,
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    hintText: 'E-mail',
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (email) {
+                    if (!emailValid(email)) {
+                      return 'Invalid E-mail';
+                    } else {
+                      return null;
+                    }
+                  },
                 ),
-                keyboardType: TextInputType.emailAddress,
-                validator: (email) {
-                  return null;
-                },
-              ),
-              const SizedBox(
-                height: 16.9,
-              ),
-              TextFormField(
-                autocorrect: false,
-                decoration: const InputDecoration(
-                  hintText: 'Password',
+                const SizedBox(
+                  height: 16.9,
                 ),
-                obscureText: true,
-                validator: (password) {
-                  if (password.isEmpty || password.length < 6) {
-                    return 'Invalid Password';
-                  } else {
-                    return null;
-                  }
-                },
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: FlatButton(
-                  onPressed: () {},
-                  padding: EdgeInsets.zero,
-                  child: const Text('Forgot my password'),
+                TextFormField(
+                  autocorrect: false,
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                    hintText: 'Password',
+                  ),
+                  obscureText: true,
+                  validator: (password) {
+                    if (password.isEmpty || password.length < 6) {
+                      return 'Invalid Password';
+                    } else {
+                      return null;
+                    }
+                  },
                 ),
-              ),
-              const SizedBox(
-                height: 16.9,
-              ),
-              SizedBox(
-                height: 45.0,
-                child: RaisedButton(
-                  onPressed: () {},
-                  color: Theme.of(context).primaryColor,
-                  textColor: Colors.white,
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 18.0,
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.zero,
+                    child: const Text('Forgot my password'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16.9,
+                ),
+                SizedBox(
+                  height: 45.0,
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (formKey.currentState.validate()) {
+                        print(emailController.text);
+                        print(passwordController.text);
+                      }
+                    },
+                    color: Theme.of(context).primaryColor,
+                    textColor: Colors.white,
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
