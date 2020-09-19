52d298a29cf12ab3e0049a7b5b0c544dcd344f8e
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-09-19 16:51:25 -0300

    seeking sizes in firebase

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,15 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import 'item_size.dart';
+
 class Product {
   String id;
   String name;
   String description;
   List<String> images;
+  List<ItemSize> sizes;
 
   Product.fromDocument(DocumentSnapshot document) {
     id = document.id;
     name = document.data()['name'] as String;
     description = document.data()['description'] as String;
     images = List<String>.from(document.data()['images'] as List<dynamic>);
+    sizes = (document.data()['sizes'] as List<dynamic> ?? []).map((size) {
+      return ItemSize.fromMap(size as Map<String, dynamic>);
+    }).toList();
   }
 }
