5e509d39ada6b9e5df459ece6dcc8df91e6204d5
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-03-06 19:44:24 -0300

    adding and removing sizes

--- lib/screens/edit_product/components/edit_item_size.dart
+++ lib/screens/edit_product/components/edit_item_size.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:virtual_store/common/custom_icon_button.dart';
 import 'package:virtual_store/models/item_size.dart';
 
 class EditItemSize extends StatelessWidget {
   final ItemSize size;
+  final VoidCallback onRemove;
 
-  const EditItemSize({this.size});
+  const EditItemSize({this.size, this.onRemove});
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Expanded(
+          flex: 30,
           child: TextFormField(
             initialValue: size.name,
             decoration: const InputDecoration(
@@ -23,8 +26,9 @@ class EditItemSize extends StatelessWidget {
           width: 4,
         ),
         Expanded(
+          flex: 30,
           child: TextFormField(
-            initialValue: size.stock.toString(),
+            initialValue: size.stock?.toString(),
             decoration: const InputDecoration(
               labelText: 'Stock',
               isDense: true,
@@ -36,14 +40,29 @@ class EditItemSize extends StatelessWidget {
           width: 4,
         ),
         Expanded(
+          flex: 40,
           child: TextFormField(
-            initialValue: size.price.toStringAsFixed(2),
+            initialValue: size.price?.toStringAsFixed(2),
             decoration: const InputDecoration(
               labelText: 'Price',
               isDense: true,
+              prefixText: 'U\$',
             ),
             keyboardType: const TextInputType.numberWithOptions(decimal: true),
           ),
+        ),
+        CustomIconButton(
+          iconData: Icons.remove,
+          color: Colors.red,
+          onTap: onRemove,
+        ),
+        const CustomIconButton(
+          iconData: Icons.arrow_drop_up,
+          color: Colors.black,
+        ),
+        const CustomIconButton(
+          iconData: Icons.arrow_drop_down,
+          color: Colors.black,
         ),
       ],
     );
